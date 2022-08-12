c3b6e52a72c9bdffc485ad45a92c3def28d87c97
Ray Maldonado <vivachihuahua2004@gmail.com>
2022-08-12 14:52:51 -0500

    Initial Commit

---
+++ src/PrimitiveDataTypes/PrimitiveFundamentals.java
@@ -0,0 +1,24 @@
+package PrimitiveDataTypes;
+
+// There are 4 main Primivite Data Types
+// Boolean, int, double, and char
+
+// The rest are the Reference Types
+
+public class PrimitiveFundamentals {
+
+    public static void main(String[] args){
+
+        int i = 0;
+        boolean yes = true;
+        char myChar = 'C';
+        double myDouble = 78.99;
+
+        System.out.println("int value is: " + i);
+        System.out.println("boolean value : " + yes);
+        System.out.println("char value : " + myChar);
+        System.out.println("double value is: " + myDouble);
+
+
+    }
+}
